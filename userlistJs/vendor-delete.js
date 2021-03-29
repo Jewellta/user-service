b338c7e99466da34f2151b3c706ad1f30dbe2cec
Tyler Jewell <jewelltyler15@gmail.com>
2021-03-28 23:11:07 -0400

    add

--- userlistJs/vendor-delete.js
+++ userlistJs/vendor-delete.js
@@ -14,4 +14,18 @@ $().ready(()=>{
         });
     });
 
+    $("#Delete").click(()=>{
+
+    })
+
 })
+
+
+const display =(vendor) =>{
+    console.log("display");
+    $("#pid").val(vendor.id);
+    $("#pcode").val(vendor.code);
+    $("#pname").val(vendor.name);
+    $("#paddress").val(vendor.address);
+    $("#pemail").val(vendor.email?? "na");
+    $("#pphone").val(vendor.phone?? "na");}
